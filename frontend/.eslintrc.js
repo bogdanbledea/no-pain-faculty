5b577e2f64beade81db813e90403be032d78cb77
Bogdan Bledea <bledeabogdanalexandru@gmail.com>
2020-06-06 19:19:03 +0300

    Initial project structure for backend and frontend

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 11,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+  },
+  settings:{
+    react:{
+      version: 'detect'
+    }
+    
+  }
+}
