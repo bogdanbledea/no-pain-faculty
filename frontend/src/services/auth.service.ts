b1d58c9303b6e43f172bf458c7cd883ae53327b6
Bogdan Bledea <bledeabogdanalexandru@gmail.com>
2020-07-02 00:36:02 +0300

    Refactor backend to use GraphQL

--- frontend/src/services/auth.service.ts
+++ frontend/src/services/auth.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import jwt_decode from 'jwt-decode';
 
-const API_URL = "http://localhost:8080/api/auth/";
+const API_URL = "http://localhost:4000/api/auth/";
 
 class AuthService {
   login(username:string, password:string) {
