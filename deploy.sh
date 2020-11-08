183d159ba3d7be789a9fe44fcb9f47e31a7a8104
Bogdan Bledea <bledeabogdanalexandru@gmail.com>
2020-11-08 18:33:29 +0200

    Update the deploy.sh to update frontend

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 kubectl  apply -f ./k8s/
 echo "The commit hash is ${TRAVIS_COMMIT::7}"
-kubectl set image deployment/backend backend=registry.gitlab.com/bogdanbledea/no-pain-faculty:frontend-${TRAVIS_COMMIT::7} --record
+kubectl set image deployment/frontend frontend=registry.gitlab.com/bogdanbledea/no-pain-faculty:frontend-${TRAVIS_COMMIT::7} --record
