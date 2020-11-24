1b1f3daca18de867fba5b060faaac24cb4a95ae5
Bogdan Bledea <bledeabogdanalexandru@gmail.com>
2020-11-24 21:32:05 +0200

    Fix typo in refresh token url

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,3 +1,3 @@
 export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || `http://${window.location.hostname}:31234`;
 export const GRAPHQL_URL = process.env.REACT_APP_GRAPHQL_URL || `http://${window.location.hostname}:31234/graphql`;
-export const REFRESH_TOKEN_URL = process.env.REACT_APP_TOKEN_URL || `http://${window.location.hostname}:31234/refresh_token'`;
+export const REFRESH_TOKEN_URL = process.env.REACT_APP_TOKEN_URL || `http://${window.location.hostname}:31234/refresh_token`;
